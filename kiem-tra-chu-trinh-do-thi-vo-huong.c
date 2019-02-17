c6e730ea11768ebe780abba4131002ed50636ed4
taratao <31006770+taratao@users.noreply.github.com>
2019-02-18 02:57:32 +0700

    Create kiem-tra-chu-trinh-do-thi-vo-huong.c

---
+++ kiem-tra-chu-trinh-do-thi-vo-huong.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+
+#define white 0
+#define gray 2
+#define black 1
+
+#define MAX_N 100
+
+int cycle;
+int color[MAX_N];
+
+typedef struct{
+	int size;
+	int data[MAX_N];
+}List;
+
+
+void make_null_list(List* L){
+	L->size = 0;
+}
+
+void push_back(List* L, int X){
+	L->data[L->size] = X;
+	L->size++;
+}
+
+int element_at(List* L, int P){
+	return L->data[P-1];
+}
+
+
+typedef struct{
+	int n;
+	int A[MAX_N][MAX_N];
+}Graph;
+
+
+void init_graph(Graph *G, int n){
+	G->n = n;
+	int i, j;
+	for(i = 1; i <= n; i++){
+		for(j = 1; j <= n; j++){
+			G->A[i][j] = 0;
+		}
+	}
+}
+
+
+void add_edge(Graph *G, int x, int y){
+	G->A[x][y] = 1;
+	G->A[y][x] = 1;
+}
+
+int adjacent(Graph *G, int x, int y){
+	return G->A[x][y] == 1 || G->A[y][x] == 1;
+}
+
+
+List neighbours(Graph *G, int x){
+	int y;
+	List L;
+	make_null_list(&L);
+	for(y = 1; y <= G->n; y++){
+		if(x == y) continue;
+		if(adjacent(G, x, y)){
+			push_back(&L, y);
+		}
+	}
+	return L;
+}
+
+
+void dfs(Graph *G, int s, int parent){
+	color[s] = gray;
+	
+	List L = neighbours(G, s);
+	int j;
+	for(j = 1; j <= L.size; j++){
+		int y = element_at(&L, j);
+		
+		if(y == parent) continue;
+		
+		if(color[y] == gray){
+			cycle = 1;
+			return;
+		}
+		
+		if(color[y] == white){
+			dfs(G, y, s);
+		}
+	}
+	
+	color[s] = black;
+}
+
+int check_contains_cycle(Graph *G){
+	int j;
+	for(j = 1; j <= G->n; j++){
+		color[j] = white;
+	}
+	
+	cycle = 0;
+	for(j = 1; j <= G->n; j++){
+		if(color[j] == white){
+			dfs(G, j, 0);
+		}
+	}
+	return cycle;
+}
+
+int main(){
+//	freopen("dt.txt", "r", stdin);
+	int x, y, n, m;
+	Graph G;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	int i;
+	for(i = 1; i <= m; i++){
+		scanf("%d%d", &x, &y);
+		add_edge(&G, x, y);
+	}
+	
+	if(check_contains_cycle(&G)){
+		printf("YES");
+	}
+	else{
+		printf("NO");
+	}
+	
+	return 0;
+}
